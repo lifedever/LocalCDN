408936dd65c525b41300a5e887bc37182ecd1cd0
葛方帅 <gefangshuai@163.com>
2015-11-17 15:01:50 +0800

    init

---
+++ src/main/java/io/github/gefangshuai/App.java
@@ -0,0 +1,16 @@
+package io.github.gefangshuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class App extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
